8f6a9937d21bd952290e4fb607d97fef5456867a
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-04-04 17:43:30 +0300

    1 задание

--- FP_Practice_18_2.cpp
+++ FP_Practice_18_2.cpp
@@ -7,14 +7,15 @@ using namespace std;
 
 int main()
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-    CustomRectangle rect = {2, 4, 20, 7};
-    printRectangle(rect);
-    
-    
-
-    return 0;
+	CustomRectangle rect;
+	cout << "Укажите стороны прямоугольника, высота и ширина" << endl;
+	cin >> rect.height >> rect.width;
+	cout << "Укажите координаты положения прямоугольника" << endl;
+	cin >> rect.x >> rect.y;
+	printRectangle(rect);
+	return 0;
 }
 
